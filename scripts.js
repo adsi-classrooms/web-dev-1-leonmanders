43dbdfb46588cfea3b5104c1a098c59d8bdc3ee3
leonmanders <60929960+leonmanders@users.noreply.github.com>
2020-04-02 20:52:19 +0200

    adfaf

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ request.send();
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
 	document.getElementById("mySidebar").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("main").style.marginLeft = "0px";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
